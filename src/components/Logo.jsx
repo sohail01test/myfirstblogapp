8682003312e202b7dc5ff8f853bb81d9b418e182
sohail01test <sohailschool69@gmail.com>
2024-09-20 15:18:21 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      Logo
+       {/* <img src="../../public/images/logoas.png" alt="Logo" /> */}
+       </div>
+
+  )
+}
+
+export default Logo
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn"
+import Button from './Button'
+import Input from "./Input";
+import Login from './Login'
+import RTE from "./RTE";
+import Signup from "./Signup";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout";
+import Select from "./Select"
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Login,
+    RTE,
+    Signup,
+    PostCard,
+    PostForm,
+    Select,
+    AuthLayout,
+}
